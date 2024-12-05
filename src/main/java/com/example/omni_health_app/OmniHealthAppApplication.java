07ec7c643d95738a288117179dda13fc639610ae
kradib <radibkar32@gmail.com>
2024-12-05 20:15:09 +0530

    Boilerplate code set up

---
+++ src/main/java/com/example/omni_health_app/OmniHealthAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.omni_health_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniHealthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniHealthAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/omni_health_app/OmniHealthAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.omni_health_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+class OmniHealthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
